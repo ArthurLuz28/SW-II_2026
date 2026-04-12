aabd2b4b76b2f996542f50d381ff14b0b9535d9b
Arthur Luz <159718035+ArthurLuz28@users.noreply.github.com>
2026-04-12 12:38:25 -0300

    Delete aula03 directory

--- aula03/exercicio01.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $produtos = array("items"=>array("Nome"=>"RTX 6090","Preço"=>15000.00,"Quantidade"=>5));
-    $encode = json_encode($produtos);
-
-    file_put_contents("produtos.json",$encode);
-?>
